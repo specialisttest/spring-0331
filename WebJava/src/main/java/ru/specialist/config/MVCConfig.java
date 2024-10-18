6911a23047488d8330ab4d713c169aecadd2a4b8
Sergey Shuykov <sergey_x@mail.ru>
2024-10-19 01:54:41 +0300

    add thymeleaf samples to WebJava

--- WebJava/src/main/java/ru/specialist/config/MVCConfig.java
+++ WebJava/src/main/java/ru/specialist/config/MVCConfig.java
@@ -5,12 +5,20 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+
 
 @Configuration
 @EnableWebMvc
@@ -46,7 +54,7 @@ public class MVCConfig implements WebMvcConfigurer {
 	{
 		InternalResourceViewResolver viewResolver = 
 				new InternalResourceViewResolver();
-		viewResolver.setOrder(1);
+		viewResolver.setOrder(2);
 		
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
@@ -54,6 +62,47 @@ public class MVCConfig implements WebMvcConfigurer {
 		return viewResolver;
 	}
 	
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
 	
+	@Bean
+	@Description("Thymeleaf Template Resolver")
+	public SpringResourceTemplateResolver  templateResolver() {
+		SpringResourceTemplateResolver  templateResolver = new SpringResourceTemplateResolver ();
+	    templateResolver.setPrefix("/WEB-INF/thymeleaf/views/");
+	    templateResolver.setSuffix(".html");
+	    templateResolver.setTemplateMode(TemplateMode.HTML);
+	    templateResolver.setCharacterEncoding("UTF-8");
+	    templateResolver.setCacheable(false);
+
+	    return templateResolver;
+	}
+
+	@Bean
+	@Description("Thymeleaf Template Engine")
+	public SpringTemplateEngine templateEngine() {
+	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    templateEngine.setTemplateResolver(templateResolver());
+	    templateEngine.setTemplateEngineMessageSource(messageSource());
+	    
+	    return templateEngine;
+	}
+	
+	@Bean
+	@Description("Thymeleaf View Resolver")
+	public ThymeleafViewResolver viewResolver() {
+	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	    viewResolver.setTemplateEngine(templateEngine());
+	    viewResolver.setCharacterEncoding("UTF-8");
+	    
+	    viewResolver.setOrder(1);
+	    
+	    return viewResolver;
+	}
 
 }
